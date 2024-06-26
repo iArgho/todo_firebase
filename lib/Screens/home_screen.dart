b6d57a7f228039d78ba1b23b7a15573d14225191
Ar G Ho <argho.roy488@gmail.com>
2024-06-26 12:05:22 +0600

    Delete popup added

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -47,29 +47,82 @@ class _MyHomePageState extends State<HomeScreen> {
                   },
                   controlAffinity: ListTileControlAffinity.leading,
                   secondary: IconButton(
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.delete,
                       color: Colors.red,
                     ),
                     onPressed: () {
-                      // Handle the delete action here
+                      _showDeleteConfirmationDialog(context, docSnap.id);
                     },
                   ),
                 );
               },
             ),
           );
-
-// Add the _deleteTask method to handle the deletion
-          void _deleteTask(String docId) {
-            FirebaseFirestore.instance
-                .collection('yourCollectionName')
-                .doc(docId)
-                .delete();
-          }
         } else {
           return const CircularProgressIndicator();
         }
+      },
+    );
+  }
+
+  void _showDeleteConfirmationDialog(BuildContext context, String docId) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Confirmation'),
+          content: const Text('Are you sure you want to delete this item?'),
+          actions: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: const Text(
+                      'Cancel',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: const Text(
+                      'Delete',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
       },
     );
   }
