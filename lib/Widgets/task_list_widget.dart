9400aec45ad08a4d254c288693a6b6e8f322c184
Ar G Ho <argho.roy488@gmail.com>
2024-07-08 19:20:38 +0600

    Update task_list_widget.dart

--- lib/Widgets/task_list_widget.dart
+++ lib/Widgets/task_list_widget.dart
@@ -43,7 +43,7 @@ class TaskList extends StatelessWidget {
                     },
                   ),
                 );
-              },
+              },  
             ),
           );
         } else {
